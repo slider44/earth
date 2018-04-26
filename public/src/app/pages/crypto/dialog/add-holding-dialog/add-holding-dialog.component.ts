51c0eda347b46ce805b2a863bb509e53f5f40b74
Nimrod O. Abrigo <abrigonimrod@gmail.com>
2018-04-26 09:37:34 +0800

    resolve conflict

--- public/src/app/pages/crypto/dialog/add-holding-dialog/add-holding-dialog.component.ts
+++ public/src/app/pages/crypto/dialog/add-holding-dialog/add-holding-dialog.component.ts
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-import { Component, OnInit, Inject } from "@angular/core";
-import { UserComponent } from "../../../user/user.component";
-import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material";
-import { CmcService } from "../../../../services/crypto/cmc.service";
-import { Observable } from "rxjs/Observable";
-import { FormControl } from "@angular/forms";
-import {
-  startWith,
-  debounceTime,
-  distinctUntilChanged,
-  switchMap
-} from "rxjs/operators";
-import { map } from "rxjs/operator/map";
-import { Coin } from "../../../../models/coin";
-import { Transaction } from "../../../../models/transaction";
-=======
 import { Component, OnInit, Inject } from '@angular/core';
 import { UserComponent } from '../../../user/user.component';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
@@ -26,10 +9,6 @@ import { map } from 'rxjs/operator/map';
 import { Coin } from '../../../../models/coin';
 import { Transaction } from '../../../../models/transaction';
 import { ViewHoldingDialogComponent } from '../view-holding-dialog/view-holding-dialog.component';
-<<<<<<< HEAD
->>>>>>> 29c7dbda72fd95857f1f143dab0a624d093a2254
-=======
->>>>>>> 29c7dbda72fd95857f1f143dab0a624d093a2254
 
 @Component({
   selector: "app-add-holding-dialog",
@@ -38,46 +17,20 @@ import { ViewHoldingDialogComponent } from '../view-holding-dialog/view-holding-
 })
 export class AddHoldingDialogComponent implements OnInit {
   transaction = new Transaction();
-<<<<<<< HEAD
-<<<<<<< HEAD
-
   date = new FormControl(new Date());
   constructor(
     public dialogRef: MatDialogRef<UserComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) {}
-=======
-  date = new FormControl(new Date());
-=======
-  date = new FormControl(new Date());
->>>>>>> 29c7dbda72fd95857f1f143dab0a624d093a2254
-  constructor(public dialogRef: MatDialogRef<ViewHoldingDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any) { 
-      
-    }
->>>>>>> 29c7dbda72fd95857f1f143dab0a624d093a2254
 
   ngOnInit() {
     this.transaction.userID = this.data.userID
     this.transaction.time_stamp = this.date.value;
   }
-
-<<<<<<< HEAD
-  addHolding() {
-    this.dialogRef.close(this.transaction);
-  }
-
-<<<<<<< HEAD
-  onNoClick() {
-=======
   addHolding(){
     this.dialogRef.close(this.transaction);
   }
-
-=======
->>>>>>> 29c7dbda72fd95857f1f143dab0a624d093a2254
   onNoClick(){
->>>>>>> 29c7dbda72fd95857f1f143dab0a624d093a2254
     this.dialogRef.close();
   }
 }
